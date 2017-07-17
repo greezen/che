cf60dacc8dd51de440b8c88b14fa5d61ac1cf87c
greezen <852820276@qq.com>
2017-07-17 18:46:58 +0800

    20170717

--- api/im.php
+++ api/im.php
@@ -30,6 +30,35 @@ function action_default()
 
 function action_add_group()
 {
+    $access_token = helper::post('access_token');
+    $group_name = helper::post('group_name');
+    $desc = helper::post('desc');
+    $public = helper::post('public', 0, 'intval');
+    $access_data = helper::get_cache($access_token);
+
+    if (empty($access_data)) {
+        helper::json('false', '登录超时，请重新登录');
+    }
+
+    $im = Easemob::getInstance();
+    $options = array(
+        'groupname' => $group_name,
+        'desc' => $desc,
+        'public' => $public,
+        'owner' => $access_data['uid'],
+    );
+
+    $res = $im->createGroup($options);
+
+    if (!empty($res['error'])) {
+        helper::json('false', '创建群组失败');
+    }
+
+    helper::json('true', '创建群组成功');
 }
 
-?>
+function request($action, $param)
+{
+    $im = Easemob::getInstance();
+    $res = $im->$action();
+}
